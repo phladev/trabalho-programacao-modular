582831283dd2a346872e9029d3849c2b1fbc3222
phladev <phladev05@gmail.com>
2025-09-24 17:21:47 -0300

    criando classe "Entidade"

---
+++ Sistema-reserva-barbeiro/modelo/Entidade.java
@@ -0,0 +1,11 @@
+public abstract class Entidade {
+    private Intenger id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
